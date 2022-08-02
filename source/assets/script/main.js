8c0ed6bebde08f31d03b545281e8405da86a8dd4
Patrick <patrin.ii@bk.ru>
2022-08-02 19:06:56 +0300

    added a section banner

--- source/assets/script/main.js
+++ source/assets/script/main.js
@@ -1,5 +1,6 @@
+import {Swiper, Navigation, Pagination} from "swiper";
 
-
+document.addEventListener('DOMContentLoaded', ()=> {
     /*nav menu burger*/
 
     const burger = document.querySelector(".burger")
@@ -13,6 +14,39 @@
     navBody.classList.toggle('nav_active');
     burger.classList.toggle('burger_active');
     body.classList.toggle("body-fixed")
-}
+    }
     /* END ///nav menu burger*/
+
+    /* Slider banner */
+    const sliderBanner = new Swiper('.banner', {
+        modules: [ Navigation, Pagination ],
+        navigation: {
+            nextEl: '.swiper-button-prev',
+            prevEl: '.swiper-button-next',
+        
+          },
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+          spaceBetween: 0,
+          loop: true,
+          grabCursor: true,
+          enabled: false,
+        
+            breakpoints: {
+              1024: {
+                slidesPerView: 1,
+                //slidesPerGroup: 1,
+                watchOverflow: false, 
+                enabled: true,
+                loop: false,
+                grabCursor: true,
+              }, 
+            },
+    })
+    /* END ///Slider banner*/
+
+})
+
 
